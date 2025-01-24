ed8d3f85d6389b295201f20c314f2be741d72bfc
Yanni Karlaftis <yannikarlaftis@gmail.com>
2025-01-24 20:18:53 +1100

    Set-Up SQLite and C# Project

---
+++ BookingService/BookingService/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookingService
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SQLiteAccess sqliteAccess;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Initialize the SQLiteHelper with your database file path
+            sqliteAccess = new SQLiteAccess(@"C:\Users\yanni\OneDrive\Documents\Projects\BookingsServices\BookingService\BookingsDB.db");
+            LoadMovies();
+        }
+
+        // Method to load movies from the database into the DataGrid
+        private void LoadMovies()
+        {
+            string query = "SELECT * FROM Movie";  // Query to get all movies
+            DataTable moviesData = sqliteAccess.ExecuteQuery(query);
+
+            // Bind the data to the DataGrid
+            MoviesDataGrid.ItemsSource = moviesData.DefaultView;
+        }
+    }
+}
---
+++ BookingService/BookingService/SQLiteAccess.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+
+
+namespace BookingService
+{
+    public class SQLiteAccess
+    {
+        private string connectionString;
+
+        // Constructor to initialize the connection string
+        public SQLiteAccess(string databaseFile)
+        {
+            connectionString = $"Data Source={databaseFile};Version=3;";
+        }
+
+        // Open a SQLite connection
+        private SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+
+        // Method to execute a non-query (INSERT, UPDATE, DELETE)
+        public void ExecuteNonQuery(string query)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Method to retrieve data (SELECT) and return as a DataTable
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable dataTable = new DataTable();
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        dataTable.Load(reader);
+                    }
+                }
+            }
+            return dataTable;
+        }
+    }
+}
